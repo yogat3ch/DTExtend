7af6c28f78ab70751f4a772b8a5eb649cf6d79d0
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2023-12-22 18:10:12 -0500

    grunt: add watch, timestamp to banners

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,31 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
+      
+      watch: {
+        scripts: {
+          files: ['inst/srcjs/global_fns.js', 'inst/srcjs/dtextend.js'],
+          tasks: ['default'],
+          options: {
+            spawn: false,
+          },
+        },
+      },
       concat: {
         combine: {
             src: [
                 "inst/srcjs/global_fns.js",
                 "inst/srcjs/dtextend.js"
             ],
-            dest: 'inst/srcjs/combined.js'
+            dest: 'inst/srcjs/combined.js',
+            options: {
+                banner:  '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n',
+              }
         }
       },
       uglify: {
         options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %> */\n'
         },
         build: {
           src: 'inst/srcjs/combined.js',
@@ -22,11 +35,18 @@ module.exports = function(grunt) {
       }
     });
   
+    // npm install grunt --save-dev
+    // npm install grunt-contrib-watch --save-dev
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // npm install grunt-contrib-jshint --save-dev
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // npm install grunt-contrib-concat --save-dev
     grunt.loadNpmTasks('grunt-contrib-concat');
+    // npm install grunt-contrib-uglify --save-dev
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
   
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['concat','uglify']);
   
   };
