498d97ea95f19a68784ea54461916846b88164cb
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2024-05-25 20:36:07 -0400

    Add UU reimports

---
+++ R/aaa_reimports.R
@@ -0,0 +1,31 @@
+#' @title Re-imports
+#' @name Re-imports
+#' @description Useful functions from other packages
+#' @importFrom rlang `%||%` `%|%`
+#' @importFrom UU `%|0|%` `%|try|%` `%|zchar|%` `%|legit|%` `%nin%` `%allin%`
+NULL
+
+
+#' @export
+rlang::`%||%`
+
+#' @export
+rlang::`%|%`
+
+#' @export
+UU::`%|try|%`
+
+#' @export
+UU::`%|0|%`
+
+#' @export
+UU::`%|zchar|%`
+
+#' @export
+UU::`%|legit|%`
+
+#' @export
+UU::`%nin%`
+
+#' @export
+UU::`%allin%`
