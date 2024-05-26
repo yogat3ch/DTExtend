c4c9c60bb7f2a0dd884e1b757a91fcda05a6b950
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2024-05-25 20:36:36 -0400

    useDTExtend: make `tippy` dependencies optional

--- R/useDTExtend.R
+++ R/useDTExtend.R
@@ -1,16 +1,18 @@
 #' Include in the DOM to provide supporting dependencies for this package
 #'
+#' @param use_tippy \code{lgl} Whether to use the Tippy package if using `DT_header_tooltips`
 #' @return \code{None} Attaches dependency to the DOM
 #' @export
 #'
-useDTExtend <- function() {
+useDTExtend <- function(use_tippy = FALSE) {
   shiny::tagList(
     htmltools::htmlDependency(
       name = "DTExtend",
       version = utils::packageVersion("DTExtend"),
       package = "DTExtend",
       src = "srcjs",
-      script = list(src = "dtextend.min.js")
+      script = list(src = "dtextend.min.js"),
+      if (use_tippy) tippy::useTippy(),
     )
   )
 }
